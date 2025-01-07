d812bb22dd931b3e22a4713d2adbc50a12a91ce7
srivatsala <gayatri05vatsala@gmail.com>
2025-01-07 16:21:52 +0530

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,19 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { Providers } from "./providers";
+
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en"  suppressHydrationWarning>
+      <head />
+      <body>
+        <Navbar />
+          <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
